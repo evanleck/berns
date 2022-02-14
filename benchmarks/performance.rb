cfe2677d49ae21711f3ccb8cff31f0d4d4e78ab2
Evan Lecklider <evan@lecklider.com>
2022-02-14 12:45:36 -0800

    Add benchmarks/performance.rb file

---
+++ benchmarks/performance.rb
@@ -0,0 +1,59 @@
+# frozen_string_literal: true
+$LOAD_PATH.unshift File.expand_path('../lib', __dir__)
+
+require 'benchmark/ips'
+require 'berns'
+require 'cgi/escape'
+
+EMPTY = ''
+SPACE = ' '
+
+def to_attributes(attributes)
+  return EMPTY if attributes.empty?
+
+  string = +''
+
+  attributes.each do |attr, value|
+    string << SPACE
+
+    to_attribute(attr, value, string)
+  end
+
+  string.strip!
+  string
+end
+
+def to_attribute(attribute, value, string = +'')
+  if value.is_a?(FalseClass) # rubocop:disable Style/CaseLikeIf
+  # noop
+  elsif value.is_a?(TrueClass)
+    string << attribute.to_s
+  elsif value.is_a?(Hash)
+    value.each do |attr, subval|
+      string << SPACE
+
+      to_attribute(attr.nil? ? attribute : "#{ attribute }-#{ attr }", subval, string)
+    end
+
+    string.strip!
+  else
+    string << %(#{ attribute }="#{ CGI.escapeHTML(value.to_s) }")
+  end
+
+  string
+end
+
+def element(tag, attributes = nil)
+  content = yield if block_given?
+
+  "<#{ tag } #{ to_attributes(attributes) }>#{ content }</#{ tag }>"
+end
+
+ATTRS = { class: 'class', data: { attr: 'value' } }.freeze
+
+Benchmark.ips do |x|
+  x.report('element') { element('p', ATTRS) { 'Content' } }
+  x.report('berns') { Berns.element('p', ATTRS) { 'Content' } }
+
+  x.compare!
+end
