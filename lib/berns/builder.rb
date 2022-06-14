12fd37313bf34ed984c9709a097d3458714a1676
Evan Lecklider <evan@lecklider.com>
2022-06-14 12:10:02 -0700

    Make instances of Berns::Builder re-callable

--- lib/berns/builder.rb
+++ lib/berns/builder.rb
@@ -5,22 +5,28 @@ module Berns
   # An HTML builder DSL using Berns' HTML methods.
   class Builder
     def initialize(&block)
+      raise(ArgumentError, 'Berns::Builder initialized without a block argument', caller) unless block
+
       @block = block
+    end
+
+    # @return [String]
+    def call(*args, **kwargs)
       @buffer = +''
-    end
+      content = instance_exec(*args, **kwargs, &@block)
 
-    # @return [String]
-    def call(*args, **opts)
-      instance_exec(*args, **opts, &@block)
-      to_s
+      # This is a special case where the buffer hasn't been appended to but the
+      # block returned a string.
+      if @buffer.empty? && content.is_a?(String)
+        Berns.escape_html(content).freeze
+      else
+        @buffer.freeze
+      end
     end
+    alias to_s call
+    alias to_str call
 
-    # @return [String]
-    def to_s
-      @buffer.freeze
-    end
-
-    # Append text to the buffer.
+    # Append HTML escaped text to the buffer.
     #
     # @param string [String]
     # @return [String]
@@ -28,31 +34,39 @@ module Berns
       @buffer << Berns.escape_html(string.to_s)
     end
 
+    # Append raw text to the buffer.
+    #
+    # @param string [String]
+    # @return [String]
+    def raw(string)
+      @buffer << string.to_s
+    end
+
     # Append an arbitrary standard element to the buffer.
     #
     # @return [String]
-    def element(*args, **opts, &block)
-      content = Builder.new.instance_exec(*args, **opts, &block) if block
-      @buffer << Berns.element(*args, **opts) { content }
+    def element(elm, *args, **kwargs, &block)
+      content = Builder.new(&block).call if block
+      @buffer << Berns.element(elm, *args, **kwargs) { content }
     end
 
     # Append an arbitrary void element to the buffer.
     #
     # @return [String]
-    def void(*args, **opts)
-      @buffer << Berns.void(*args, **opts)
+    def void(...)
+      @buffer << Berns.void(...)
     end
 
     Berns::STANDARD.each do |meth|
-      define_method(meth) do |*args, **opts, &block|
-        content = Builder.new.instance_exec(*args, **opts, &block) if block
-        @buffer << Berns.send(meth, *args, **opts) { content }
+      define_method(meth) do |*args, **kwargs, &block|
+        content = Builder.new(&block).call if block
+        @buffer << Berns.send(meth, *args, **kwargs) { content }
       end
     end
 
     Berns::VOID.each do |meth|
-      define_method(meth) do |*args, **opts|
-        @buffer << Berns.send(meth, *args, **opts)
+      define_method(meth) do |*args, **kwargs|
+        @buffer << Berns.send(meth, *args, **kwargs)
       end
     end
   end
--- test/berns_builder_test.rb
+++ test/berns_builder_test.rb
@@ -3,18 +3,152 @@ require 'berns/builder'
 require 'minitest/autorun'
 
 describe Berns::Builder do
-  it "builds a DOM using Berns' own methods" do
-    dom = Berns::Builder.new do
-      h1 { 'Header' }
-      text "where'd this come from?!"
-      div
-      p(class: 'para') { 'A paragraph!' }
+  describe '#new' do
+    it 'raises an argument error if a block is not passed' do
+      assert_raises(ArgumentError) { Berns::Builder.new }
     end
-
-    assert_equal %(<h1>Header</h1>where&#39;d this come from?!<div></div><p class="para">A paragraph!</p>), dom.call
   end
 
-  it 'allows nesting' do
+  describe '#element' do
+    it 'allows creating plain elements' do
+      dom = Berns::Builder.new do
+        element(:div) { 'Content!' }
+      end
+
+      assert_equal '<div>Content!</div>', dom.call
+      assert_equal '<div></div>', Berns::Builder.new { element('div') }.call
+    end
+  end
+
+  describe '#void' do
+    it 'allows creating void elements' do
+      dom = Berns::Builder.new do
+        void(:br, class: 'break')
+        void(:hr)
+      end
+
+      assert_equal '<br class="break"><hr>', dom.call
+      assert_equal '<br>', Berns::Builder.new { void('br') }.call
+    end
+  end
+
+  describe '#text' do
+    it 'appends text to the internal buffer and escapes the content' do
+      dom = Berns::Builder.new do
+        text '<h1>And & this'
+      end
+
+      assert_equal '&lt;h1&gt;And &amp; this', dom.call
+    end
+  end
+
+  describe '#raw' do
+    it 'appends text to the internal buffer without escaping the content' do
+      dom = Berns::Builder.new do
+        raw '<h1>And & this'
+      end
+
+      assert_equal '<h1>And & this', dom.call
+    end
+  end
+
+  describe '#call' do
+    it 'renders the template to a string' do
+      dom = Berns::Builder.new { b { 'Bold!' } }
+      assert_equal '<b>Bold!</b>', dom.call
+    end
+
+    it 'allows passing keyword arguments and raises an error when omitted' do
+      dom = Berns::Builder.new do |title:, para:|
+        h1 { title }
+        p { para }
+      end
+
+      assert_raises(ArgumentError) { dom.call }
+      assert_equal %(<h1>TITLE!</h1><p>PARAGRAPH!</p>), dom.call(title: 'TITLE!', para: 'PARAGRAPH!')
+    end
+
+    it 'allows passing positional arguments' do
+      dom = Berns::Builder.new do |pos, pos2|
+        h1 { pos }
+        p { pos2 }
+      end
+
+      # Even though it seems like this _should_ raise an error, it doesn't
+      # because Proc objects don't raise an ArgumentError for missing arguments
+      # and instead fill in nils. Were this a lambda, it would raise an error.
+      assert_equal %(<h1></h1><p></p>), dom.call
+      assert_equal %(<h1>TITLE!</h1><p>PARAGRAPH!</p>), dom.call('TITLE!', 'PARAGRAPH!')
+    end
+
+    it 'allows passing both positional and keyword arguments and raises an error when omitted' do
+      dom = Berns::Builder.new do |pos, title:|
+        h1 { title }
+        p { pos }
+      end
+
+      assert_raises(ArgumentError) { dom.call }
+      assert_equal %(<h1>TITLE!</h1><p>PARAGRAPH!</p>), dom.call('PARAGRAPH!', title: 'TITLE!')
+    end
+  end
+
+  describe '#to_s' do
+    it 'renders the template to a string' do
+      dom = Berns::Builder.new { b { 'Bold!' } }
+      assert_equal '<b>Bold!</b>', dom.to_s
+    end
+  end
+
+  describe '#to_str' do
+    it 'renders the template to a string' do
+      dom = Berns::Builder.new { b { 'Bold!' } }
+      assert_equal '<b>Bold!</b>', dom.to_str
+    end
+  end
+
+  describe 'standard elements' do
+    Berns::STANDARD.each do |elm|
+      it "appends an element using ##{ elm }" do
+        assert_equal "<#{ elm }>Content</#{ elm }>", Berns::Builder.new { send(elm) { 'Content' } }.call
+      end
+    end
+  end
+
+  describe 'void elements' do
+    Berns::VOID.each do |elm|
+      it "appends an element using ##{ elm }" do
+        assert_equal "<#{ elm }>", Berns::Builder.new { send(elm) }.call
+      end
+    end
+  end
+
+  describe 'block return values' do
+    it 'does not capture the block return when the buffer has been modified' do
+      dom = Berns::Builder.new do
+        span { "This text is #{ b { 'bold' } }!" }
+      end
+
+      assert_equal '<span><b>bold</b></span>', dom.call
+    end
+
+    it 'does capture the block return when the buffer has not been modified' do
+      dom = Berns::Builder.new do
+        span { 'This text is plain.' }
+      end
+
+      assert_equal '<span>This text is plain.</span>', dom.call
+    end
+
+    it 'escapes the block return value when captured' do
+      dom = Berns::Builder.new do
+        span { 'This <b>text</b> is plain.' }
+      end
+
+      assert_equal '<span>This &lt;b&gt;text&lt;/b&gt; is plain.</span>', dom.call
+    end
+  end
+
+  specify 'nesting' do
     dom = Berns::Builder.new do
       p(class: 'para') do
         text 'bare text'
@@ -25,95 +159,21 @@ describe Berns::Builder do
     assert_equal %(<p class="para">bare text<span class="inline">More text!</span></p>), dom.call
   end
 
-  it 'allows passing local variables and raises an error when omitted' do
-    dom = Berns::Builder.new do |title:, para:|
-      h1 { title }
-      p { para }
+  specify "Builder instances can be #call'ed multiple times" do
+    dom = Berns::Builder.new do |name|
+      h1 { name }
     end
 
-    assert_equal %(<h1>TITLE!</h1><p>PARAGRAPH!</p>), dom.call(title: 'TITLE!', para: 'PARAGRAPH!')
-
-    assert_raises(ArgumentError) do
-      dom.call
-    end
-
-    dom = Berns::Builder.new do |pos, pos2|
-      h1 { pos }
-      p { pos2 }
-    end
-
-    assert_equal %(<h1>TITLE!</h1><p>PARAGRAPH!</p>), dom.call('TITLE!', 'PARAGRAPH!')
-
-    dom = Berns::Builder.new do |pos, title:|
-      h1 { title }
-      p { pos }
-    end
-
-    assert_equal %(<h1>TITLE!</h1><p>PARAGRAPH!</p>), dom.call('PARAGRAPH!', title: 'TITLE!')
+    assert_equal '<h1>Bob</h1>', dom.call('Bob')
+    assert_equal '<h1>Sue</h1>', dom.call('Sue')
   end
 
-  it 'has access to the scope around where it was initialized' do
-    opts = %w[one two three]
-
-    dom = Berns::Builder.new do
-      opts.each do |opt|
-        span { opt }
-      end
+  specify "Builder instances return a frozen string when #call'ed" do
+    dom = Berns::Builder.new do |name|
+      h1 { name }
     end
 
-    assert_equal %(<span>one</span><span>two</span><span>three</span>), dom.call
-  end
-
-  it 'can be initialized without a block and added to' do
-    builder = Berns::Builder.new
-    builder.h1 { 'Header' }
-
-    assert_equal '<h1>Header</h1>', builder.to_s
-  end
-
-  it 'allows interpolation nesting in strings' do
-    dom = Berns::Builder.new do
-      span { "This text is #{ b { 'bold' } }!" }
-    end
-
-    assert_equal '<span>This text is <b>bold</b>!</span>', dom.call
-  end
-
-  it 'escapes text content' do
-    dom = Berns::Builder.new do
-      text '<h1>And & this'
-    end
-
-    assert_equal '&lt;h1&gt;And &amp; this', dom.call
-  end
-
-  it 'has the element method' do
-    dom = Berns::Builder.new do
-      element('div', class: 'some-div') do
-        text 'Content!'
-      end
-    end
-
-    assert_equal '<div class="some-div">Content!</div>', dom.call
-
-    dom = Berns::Builder.new do
-      element('div')
-    end
-
-    assert_equal '<div></div>', dom.call
-  end
-
-  it 'has the void method' do
-    dom = Berns::Builder.new do
-      void('br', class: 'break')
-    end
-
-    assert_equal '<br class="break">', dom.call
-
-    dom = Berns::Builder.new do
-      void('br')
-    end
-
-    assert_equal '<br>', dom.call
+    assert_predicate dom.call('Bob'), :frozen?
+    assert_predicate dom.call('Sue'), :frozen?
   end
 end
